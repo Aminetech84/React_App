be9713e74dcef4929b47fb82be833d056b7eeb1d
Amine Ouldaouali <aminetech84@hotmail.com>
2019-08-22 08:52:12 +0100

    Updates

---
+++ precache-manifest.fd5b47f2bbd878707eba0ef95f97ec1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64977d806930a77f6be8970d4558310",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a8a62f2be5bf69300e7d",
+    "url": "/react-tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "8fae9c34cab20d308188",
+    "url": "/react-tutorial/static/js/2.0329cd26.chunk.js"
+  },
+  {
+    "revision": "a8a62f2be5bf69300e7d",
+    "url": "/react-tutorial/static/js/main.1d73f86e.chunk.js"
+  },
+  {
+    "revision": "81c7aa261da46a39678f",
+    "url": "/react-tutorial/static/js/runtime~main.bb8882cb.js"
+  }
+]);
