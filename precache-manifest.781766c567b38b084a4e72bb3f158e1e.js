101f39b216bb49b2f07a150e5a07525bf7d865cc
Amine Ouldaouali <aminetech84@hotmail.com>
2019-08-22 08:53:46 +0100

    Updates

---
+++ precache-manifest.781766c567b38b084a4e72bb3f158e1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d40328f54f80bf86281e13c3a13b52",
+    "url": "/React_App/index.html"
+  },
+  {
+    "revision": "a8a62f2be5bf69300e7d",
+    "url": "/React_App/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "8fae9c34cab20d308188",
+    "url": "/React_App/static/js/2.0329cd26.chunk.js"
+  },
+  {
+    "revision": "a8a62f2be5bf69300e7d",
+    "url": "/React_App/static/js/main.1d73f86e.chunk.js"
+  },
+  {
+    "revision": "6427fb5fb39fb6d8ac0d",
+    "url": "/React_App/static/js/runtime~main.70c7b2f9.js"
+  }
+]);
